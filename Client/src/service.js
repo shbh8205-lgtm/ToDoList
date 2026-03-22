361470c163b11118e7e876bb9d0bfc08b21c8c99
shbh8205-lgtm <shbh8205@gmail.com>
2026-03-22 21:04:56 +0200

    Update service.js

--- Client/src/service.js
+++ Client/src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_API_ADRESS || process.env.REACT_APP_API_URL;
+axios.defaults.baseURL = (process.env.REACT_APP_API_ADRESS || process.env.REACT_APP_API_URL).trim();
 
 console.log("the server: "+axios.defaults.baseURL);
 console.log("All env vars:", process.env);
